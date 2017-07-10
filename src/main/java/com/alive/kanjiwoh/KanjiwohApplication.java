1b16b3f70d50a755ce5cb70436c49bf54a5a3f09
doyeonhan <yeondev@gmail.com>
2017-07-10 22:32:21 +0900

    lets go

---
+++ src/main/java/com/alive/kanjiwoh/KanjiwohApplication.java
@@ -0,0 +1,12 @@
+package com.alive.kanjiwoh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanjiwohApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanjiwohApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alive/kanjiwoh/KanjiwohApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alive.kanjiwoh;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KanjiwohApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
